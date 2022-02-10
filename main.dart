91a8eec9a5474e470abbb83b6170b4569dbaf26c
Royce24vaz <91864597+Royce24vaz@users.noreply.github.com>
2022-02-10 23:22:46 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,56 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(Cmdo());
+
+class Cmdo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  late WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        if (await controller.canGoBack()) {
+          controller.goBack();
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            child: WebView(
+              initialUrl: 'https://consultmydoctoronline.com',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (controller) {
+                this.controller = controller;
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
